c4990bd17a96613d4c14ab589d03d2fcfaf386f5
aamsoumi <amir.umassd@gmail.com>
2024-03-27 22:40:38 -0400

    docker selenium

--- Selenium_exercise/test/invalidUser.spec.js
+++ Selenium_exercise/test/invalidUser.spec.js
@@ -1,5 +1,7 @@
-const { Builder, By, until, Key } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
+const { Builder, By, until,Capabilities, Key } = require('selenium-webdriver');
+
+let capabilities = Capabilities.chrome();
+
 
 const server_url = 'http://localhost:3000';
 
@@ -8,29 +10,34 @@ describe('Web App Tests', function () {
 
     before(async function () {
         // Create a new instance of the Chrome driver
-        driver = await new Builder().forBrowser('chrome').build();
-    });
+        driver = await new Builder()
+        .usingServer('http://localhost:4444')
+        .withCapabilities(capabilities)
+        await driver.get(server_url);
+    },30000);
 
     after(async function () {
         await driver.quit();
     });
 
     it('should fail to login with invalid credentials', async function () {
-        await driver.get(server_url);
+        
         await driver.findElement(By.id('username')).sendKeys('invalid_user');
         await driver.findElement(By.id('password')).sendKeys('invalid_pass');
         await driver.findElement(By.id('login')).click();
         
         // Adding a short sleep to allow for error message to appear (adjust time if needed)
-        await driver.sleep(1000);
         
+        await driver.sleep(1000);  
         // Check if error message is displayed
         try {
             await driver.wait(until.elementLocated(By.className('error-message')), 10000);
             console.log('Error message for invalid login found');
+      
         } catch (err) {
             throw new Error('Expected error message for invalid login not found');
+          
         }
-    });
+    }).timeout(5000);
 
 });
